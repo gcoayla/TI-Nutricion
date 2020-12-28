25658d47746f5edd407f814128b39d8a542117c0
gcoayla <gcoayla.28@gmail.com>
2020-12-28 03:53:14 -0500

    eliminada carpeta antigua

--- Nutricion/resources/views/home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts/template')
-
-@section('title', 'Home')
-
-@section('content')
-    <h1>Bienvenido a la página principal</h1>
-    <h2>Nutrición</h2>
-@endsection
--- Nutricion/resources/views/usuarios/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts/template')
-
-@section('title', 'Usuarios')
-
-@section('content')
-    <h1>Bienvenido a la página de usuarios</h1>
-@endsection
